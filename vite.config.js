8a1f0087347c5980a7b7a3467faf6a06a5ac34a2
Bhushan217 <bhushan217@gmail.com>
2024-08-24 08:50:45 -0400

    gh-pages -10

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,16 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), visualizer({
-    emitFile: true,
-    filename: "stats.html",
-  })],
+  base:'',
+  plugins: [svelte(), visualizer()],
+  renderBuiltUrl(filename, { hostType }) {
+    if (hostType === 'js') {
+      return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` }
+    } else {
+      return { relative: true }
+    }
+  },
+  build: {
+    outDir: './dist/basic-site',
+  }
 })
